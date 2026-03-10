c9b9eb947bea40e0b9e137a883cd4eda53a3d70e
Pedro Henrique <phlferezin03@gmail.com>
2026-03-10 09:12:14 -0300

    feat: terceira aula

--- app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/MainActivity.kt
@@ -1,9 +1,15 @@
 package br.edu.ifsp.scl.sdm.entityservicecommunication
 
+import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -16,6 +22,15 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var incrementServiceIntent: Intent
     private var counter = 0
+    private val incrementBroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            intent?.getIntExtra("VALUE", -1)?.also { value ->
+                counter = value
+                Toast.makeText(this@MainActivity, "You clicked $counter times.", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,10 +47,21 @@ class MainActivity : AppCompatActivity() {
                 })
             }
         }
-        InterEntityCommunication.valueLiveData.observe(this) { value ->
-            counter = value;
-            Toast.makeText(this, "You clicked $counter times.", Toast.LENGTH_SHORT).show()
-        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    override fun onStart() {
+        super.onStart()
+        registerReceiver(
+            incrementBroadcastReceiver,
+            IntentFilter("INCREMENT_VALUE_ACTION"),
+            Context.RECEIVER_EXPORTED
+        )
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(incrementBroadcastReceiver)
     }
 
     override fun onDestroy() {
